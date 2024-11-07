7d49ecd0f377e3023ab0cef7549de219ea03c22e
DmitryAndrosenko25 <dmitriyandrosenko@gmail.com>
2024-11-07 17:01:31 +0200

    07.11.2024 Дописал разогрев до 2000 от стартера до нагрева
    поиск адресов работает идеально.... пока что

---
+++ fin3/reactorUpTo2000.lua
@@ -0,0 +1,102 @@
+local reactorHeating = {}
+
+local component = require("component")
+local reactorInit = require("reactorInit")
+local adresses = reactorInit.getGatesAddresses()
+local reactor = component.proxy(adresses[1]) -- Подключение к реактору и гейтам
+local fluxInGate = component.proxy(adresses[2])
+local fluxOutGate = component.proxy(adresses[3])
+
+local function rInfoHeating(info) --- на вход параметр реактора в string ..на выход значение 
+	local st = reactor.getReactorInfo()
+	return st[info]
+end
+
+local function starter()
+	-- local isStarted = false
+	print("Реактор, видимо, не в статусе разогрева, а в статусе " .. rInfoHeating("status"))
+    reactor.chargeReactor() -- ВКЛ разогрев реактора
+	while rInfoHeating("status") ~= "warming_up" do
+		print("Стартер реактора гудит, пердит...")
+		reactor.stopReactor() -- stopReactor
+		reactor.chargeReactor() -- ВКЛ разогрев реактора
+		os.sleep(1)
+	end	
+	print("Стартер отработал на ура. Статус реактора " .. rInfoHeating("status"))
+	-- isStarted = true
+	-- return isStarted
+end
+
+local function charge50field() -- Заряд щита	
+	print(rInfoHeating("status"))
+	fluxInGate.setOverrideEnabled(true) -- Разрешение на управление входящим гейтом программе 
+	fluxInGate.setFlowOverride(1)
+	os.sleep(0.05)
+	local fieldCurrent = rInfoHeating("fieldStrength")
+	local field50Max = (rInfoHeating("maxFieldStrength")/2)
+	while ((fieldCurrent + 1) <= field50Max) do
+		fluxInGate.setFlowOverride(((field50Max - fieldCurrent) / 10) + 10 )
+		os.sleep(0.05) -- Ждем 
+		fieldCurrent = rInfoHeating("fieldStrength")
+	end
+	fluxInGate.setFlowOverride(0)
+	print("Щит реактора заряжен на 50%")
+end
+
+local function charge50saturation() -- Заряд Сатурации
+	print("Начало сатурации")
+	local saturationCurrent = rInfoHeating("energySaturation")
+	local saturation50Max = (rInfoHeating("maxEnergySaturation")/2)
+	if (saturationCurrent < saturation50Max) then
+		fluxInGate.setFlowOverride(1)
+		os.sleep(0.05)
+		while ((saturationCurrent + 1) < saturation50Max) do
+			saturationCurrent = rInfoHeating("energySaturation")
+			fluxInGate.setFlowOverride(((saturation50Max - saturationCurrent) / 40) + 10 )
+			os.sleep(0.05) -- Ждем 
+			saturationCurrent = rInfoHeating("energySaturation")
+		end
+	fluxInGate.setFlowOverride(0)
+	print("Насыщение реактора - 50%")	
+	else
+		print("Насыщение реактора достаточное для старта")
+	end	
+end
+
+local function heating()
+	local tCurrent = rInfoHeating("temperature")
+	local tDelta = 0
+	fluxInGate.setFlowOverride(100)
+	local inFlow = fluxInGate.getFlow()	
+	if tCurrent < 2000 then
+		while tDelta < 0.1 do
+			local tStart = rInfoHeating("temperature")
+			os.sleep(0.05) -- Ждем 
+			local tEnd = rInfoHeating("temperature")
+			fluxInGate.setFlowOverride(inFlow * 1.15) --1,1
+			inFlow = fluxInGate.getFlow()
+			tDelta = tEnd - tStart            
+		end
+	end
+	while tCurrent < 2000 do
+		fluxInGate.setFlowOverride((((2000 - tCurrent) / 2) * inFlow) + 1)
+		os.sleep(0.05) -- Ждем 
+		tCurrent = rInfoHeating("temperature")
+	end
+fluxInGate.setFlowOverride(0)
+print("Ядро разогрето до 2000 градусов и готово к запуску")
+end
+
+function reactorHeating.to2000()    
+    if(rInfoHeating("status") ~= "warming_up") then --Если не греется, то юзаем стартер
+		starter()	
+	end		
+	if (rInfoHeating("maxFieldStrength")/2) > rInfoHeating("fieldStrength") then
+		charge50field() --  вызов функции заряда щита
+	end
+	if (rInfoHeating("maxEnergySaturation")/2) > rInfoHeating("energySaturation") then
+		charge50saturation() --вызов функции сатурации
+	end	
+    heating() --  вызов функции разогрева реактора
+end
+return reactorHeating
--- fin3/test.lua
+++ fin3/test.lua
@@ -1,6 +1,6 @@
 local component = require("component")
 local reactorInit = require("reactorInit")
--- local reactorHeating = require("reactorHeating")
+local reactorUpTo2000 = require("reactorUpTo2000")
 -- local reactorShield = require("shield")
 
 local a = reactorInit.getGatesAddresses() -- Получаем адреса по порядку: 1 реактор, 2 гейт вход, 3 гейт выход
@@ -11,7 +11,21 @@ print("fluxInAddress ="..fluxInAddress)
 local fluxOutAddress = a[3]
 print("fluxOutAddress ="..fluxOutAddress) --temporary///////////////////
 
--- local reactor = component.proxy(reactorAddress)
--- local fluxIn = component.proxy(fluxInAddress)
--- local fluxOut = component.proxy(fluxOutAddress)
+local reactor = component.proxy(reactorAddress)
+local fluxIn = component.proxy(fluxInAddress)
+local fluxOut = component.proxy(fluxOutAddress)
+
+local function rInfo(info) --- на вход параметр реактора в string ..на выход значение 
+	local st = reactor.getReactorInfo()
+	return st[info]
+end
+
+
+if (rInfo("temperature") < 2000) then
+	print("В потоке тест стартую поднятие температуры до 2000")
+	reactorUpTo2000.to2000()
+	print("Программа отработала ДО КОНЦА")	
+end
+
+
 
