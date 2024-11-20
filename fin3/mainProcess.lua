ae8aeaaafae3474bdb50362637db29cfa13fb787
DmitryAndrosenko25 <dmitriyandrosenko@gmail.com>
2024-11-21 00:12:52 +0200

    little push...
    Думаю, что еще чуток....

--- fin3/mainProcess.lua
+++ fin3/mainProcess.lua
@@ -75,6 +75,86 @@ local function main(temerature)
         os.sleep(0.05)
         tEnd = rInfo("temperature")
 		
+		
+		if tMax > tCurrent then
+			if (tMax - tCurrent) > 0.01 then
+				
+				
+				if (tEnd - tStart) > 1 then						-- Если рост слишком быстрый?????
+					up = up - (1 * ((tEnd - tStart)* 75)) --50
+				elseif (tEnd - tStart) > 0.1 then
+					up = up - (1 * ((tEnd - tStart)* 50)) --50
+				elseif (tEnd - tStart) > 0.01 then
+					up = up - (1 * ((tEnd - tStart)* 25)) --50
+				else
+					up = up + 1
+				end
+			end
+			fluxOut.setFlowOverride(rInfo("generationRate") + up)
+		
+		elseif tCurrent > tMax then
+			if (tCurrent - tMax) > 0.01 then
+				
+				if (tStart - tEnd) > 100 then							-- Если падение слишком быстрое???
+					down = down - (1 * ((tStart - tEnd) * 5000000))
+				elseif (tStart - tEnd) > 10 then
+					down = down - (1 * ((tStart - tEnd) * 500000))
+				elseif (tStart - tEnd) > 1 then
+					down = down - (1 * ((tStart - tEnd) * 50000))
+				elseif (tStart - tEnd) > 0.5 then
+					down = down - (1 * ((tStart - tEnd) * 5000))
+				elseif (tStart - tEnd) > 0.1 then
+					down = down - (1 * ((tStart - tEnd) * 1000)) --500
+				elseif (tStart - tEnd) > 0.05 then
+					down = down - (1 * ((tStart - tEnd) * 500)) --300 
+				elseif (tStart - tEnd) > 0.03 then
+					down = down - (1 * ((tStart - tEnd) * 300)) --150
+				elseif (tStart - tEnd) > 0.02 then
+					down = down - (1 * ((tStart - tEnd) * 200))	--100				-- ИЛИ ТУТ ЕЩЕ ДОБАВИТЬ ЕЛС ИФ ГРАДАЦИЮ 0.05, 0.03, 0.02....
+				elseif (tStart - tEnd) > 0.01 then								-- 100  НУЖНО ПОПРОБОВАТЬ 100 - ТАК КАК ПРИ ТЕСТИРОВАНИИ 13 000 ГРАДУСОВ СКАКАЛО В ПРЕДЕЛАХ 0.1 ГРАДУСА
+					down = down - (1 * ((tStart - tEnd) * 100)) --50					
+				else
+					down = down + 1
+				end
+			end	
+			fluxOut.setFlowOverride(rInfo("generationRate") - down)
+		else
+			fluxOut.setFlowOverride(rInfo("generationRate"))
+			down = down * 0.95
+			up = 0
+		end
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		--[[
+		
 		if (tMax - tCurrent) > 0.01 then
 			-- 13000 - 12987.01 = 12,99
 		
@@ -83,18 +163,37 @@ local function main(temerature)
 			
 			if (tStart - tEnd) > 0.01 then
 				
-				print("((tStart - tEnd)/1000) = ".. ((tStart - tEnd)/1000))					-- это в принципе работает, но делитель нужно подобрать, так как температура всеравно падает
-				fluxOut.setFlowOverride(outFlow * (1 + ((tStart - tEnd)/1000)))
+				if (tMax - tCurrent) > 1 then
+					print("((tStart - tEnd)/100) = ".. ((tStart - tEnd)/100))		--1000			-- это в принципе работает, но делитель нужно подобрать, так как температура всеравно падает
+					fluxOut.setFlowOverride(outFlow * (1 + ((tStart - tEnd)/100)))
+				elseif (tMax - tCurrent) > 0.1 then
+					print("((tStart - tEnd)/300) = ".. ((tStart - tEnd)/250))		--1000			-- это в принципе работает, но делитель нужно подобрать, так как температура всеравно падает
+					fluxOut.setFlowOverride(outFlow * (1 + ((tStart - tEnd)/250)))
+				else
+					print("((tStart - tEnd)/150) = ".. ((tStart - tEnd)/300))		--1000			-- это в принципе работает, но делитель нужно подобрать, так как температура всеравно падает
+					fluxOut.setFlowOverride(outFlow * (1 + ((tStart - tEnd)/300)))
+				end
 			end	
-			
-			
 			
 			-- print("((tMax - tCurrent)/100) = ".. ((tMax - tCurrent)/1000))
 			-- fluxOut.setFlowOverride(outFlow * (1 + ((tMax - tCurrent)/1000)))
 		
+		elseif (tCurrent - tMax) > 0.01 then
+			
+			if (tStart - tEnd) > 0.01 then
+				
+				print("((tStart - tEnd)/1500) = ".. ((tStart - tEnd)/1500))		--1000		ДИКИЙ РОСТ!!!!!!	-- это в принципе работает, но делитель нужно подобрать, так как температура всеравно падает
+				fluxOut.setFlowOverride(outFlow * (1 - ((tStart - tEnd)/1500)))
+			end
+		
+		
+		
+		
+		else
+			fluxOut.setFlowOverride(rInfo("generationRate"))
 		
 		end
-		
+		--]]
 		--[[
 		
 		
